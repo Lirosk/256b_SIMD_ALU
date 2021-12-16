d63fd6497b34d0b591c6831c85aa86ec963dedea
Lirosk <kirill_crysis@bk.ru>
2021-12-17 00:51:01 +0300

    small changes

--- CONSTS.py
+++ CONSTS.py
@@ -3,21 +3,22 @@ from enum import Enum
 class ALU_FUNCS(Enum):
     NONE = 0
 
-    ADD = 1
-    SUB = 2
-    MUL = 3
-    DIV = 4
+    ADD  = 1
+    SUB  = 2
+    MUL  = 3
+    DIV  = 4
 
-    EQ = 5
+    EQ   = 5
     MORE = 6
     LESS = 7
 
-    SHL = 8
-    SHR = 9
+    SHL  = 8
+    SHR  = 9
 
 
 class DATA_TYPES(Enum):
-    pckd_b = 0
-    pckd_w = 1
+    pckd_b  = 0
+    pckd_w  = 1
     pckd_dw = 2
     pckd_qw = 3
+
